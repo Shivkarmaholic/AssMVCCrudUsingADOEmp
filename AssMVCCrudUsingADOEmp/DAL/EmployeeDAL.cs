1377f1c95e175ad499bb45647c685d094c659118
Shivkarmaholic <kharadkarsandeep1@gmail.com>
2022-07-19 01:37:40 +0530

    Add project files.

---
+++ AssMVCCrudUsingADOEmp/DAL/EmployeeDAL.cs
@@ -0,0 +1,98 @@
+﻿using AssMVCCrudUsingADOEmp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace AssMVCCrudUsingADOEmp.DAL
+{
+    public class EmployeeDAL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public EmployeeDAL()
+        {
+            con = new SqlConnection(Startup.ConnectionString);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            string query = "select * from employees";
+            cmd=new SqlCommand(query,con);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Employee employee = new Employee();
+                employee.Id = Convert.ToInt32(dr["eid"]);
+                employee.Name = dr["ename"].ToString();
+                employee.Salary = Convert.ToInt32(dr["esalary"]);
+                employees.Add(employee);
+
+            }
+            con.Close();
+
+            return employees;
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            Employee e1 = new Employee();
+            string query = "select * from employees where eid=@id";
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {                    
+                    e1.Id = Convert.ToInt32(dr["eid"]);
+                    e1.Name = dr["ename"].ToString();
+                    e1.Salary = Convert.ToInt32(dr["esalary"]);
+
+                }
+                con.Close();
+            }
+            return e1;
+        }
+
+        public int AddEmployee(Employee e)
+        {
+            string query = "insert into employees values(@name,@salary)";
+            cmd=new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@name", e.Name);
+            cmd.Parameters.AddWithValue("@salary", e.Salary);
+            con.Open();
+            int result=cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+
+        public int UpdateEmployee(Employee e)
+        {
+            string query = "update employees set ename=@name esalary=@salary";
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", e.Id);
+            cmd.Parameters.AddWithValue("@name",e.Name);
+            cmd.Parameters.AddWithValue("@salary", e.Salary);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+
+        public int DeleteEmployee(int id)
+        {
+            string query = "delete from employees where eid=@id";
+            cmd= new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id",id);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+    }
+}
---
+++ AssMVCCrudUsingADOEmp/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AssMVCCrudUsingADOEmp.Models
+{
+    [Table("employee")]
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Name Required")]
+        [DataType(DataType.Text)]
+        [Display(Name ="Employee Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Salary Required")]
+        [DataType(DataType.Currency)]
+        public int Salary { get; set; }
+    }
+}
